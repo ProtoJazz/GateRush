1e8edf39461ceba46838404cb4b4a5cbae814ddf
Jim MacKenzie <protojazz@gmail.com>
2025-06-30 18:28:31 -0500

    more release

--- lib/gate_rush/release.ex
+++ lib/gate_rush/release.ex
@@ -4,16 +4,21 @@ defmodule GateRush.Release do
   alias GateRush.Repo
 
   def migrate do
-    Application.load(:gate_rush)
+    load_app()
 
     for repo <- Application.fetch_env!(:gate_rush, :ecto_repos) do
       IO.puts("Running migrations for #{inspect(repo)}...")
+      {:ok, _} = repo.start_link()
       Migrator.run(repo, migrations_path(repo), :up, all: true)
     end
   end
 
+  defp load_app do
+    Application.load(:gate_rush)
+  end
+
   defp migrations_path(repo) do
-    app = Keyword.fetch!(repo.config, :otp_app)
+    app = Keyword.fetch!(repo.config(), :otp_app)
     priv_dir = :code.priv_dir(app)
     Path.join([priv_dir, "repo", "migrations"])
   end
